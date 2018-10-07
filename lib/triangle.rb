936a3e22819786d51a672bdf2b20774314665a68
AT Williams <aderiannaw@yahoo.com>
2018-10-07 19:10:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+
 class Triangle
-  # write code here
-end
+  
+  attr_reader :side_1, :side_2, :side_3
+  
+    
+     def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+     end
+     
+     def kind 
+       sides = [self.side_1, self.side_2, self.side_3] 
+       sides.sort!
+       if 
+    end
+  end
+    
+class TriangleError < StandardError
+    end
+     raise TriangleError
+    
+
